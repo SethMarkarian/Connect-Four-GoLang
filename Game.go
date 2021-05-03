66cb3b553d51f6c3bfe8cd89ea0e53fd6308cc08
Seth Markarian <seth.h.markarian@gmail.com>
2021-05-02 20:11:22 -0400

    added printBoard

--- Game.go
+++ Game.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 type Game struct {
 	board      [][]string
 	playerTurn int
 }
 
 func createGame() Game {
-	var newGame = Game{}
+	newGame := Game{}
 	newGame.board = [][]string{
 		{"_", "_", "_", "_", "_", "_", "_"},
 		{"_", "_", "_", "_", "_", "_", "_"},
@@ -17,4 +19,15 @@ func createGame() Game {
 	}
 	newGame.playerTurn = 1
 	return newGame
+}
+
+func printBoard(game Game) {
+	for i := 0; i < 6; i++ {
+		fmt.Println(game.board[i])
+	}
+}
+
+func main() {
+	g := createGame()
+	printBoard(g)
 }
