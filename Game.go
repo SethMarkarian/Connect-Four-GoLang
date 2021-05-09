fdeee79369242675eaed6a27276f293d61f9d6a7
Seth Markarian <seth.h.markarian@gmail.com>
2021-05-09 14:47:34 -0400

    Comments

--- Game.go
+++ Game.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 )
 
+// Creates Game structure
 type Game struct {
-	board          [][]string
-	piecesInColumn []int
-	done           bool
+	board          [][]string // Slice that holds '_', 'X', or 'O'
+	piecesInColumn []int      // Slice that counts amount of pieces in given column
+	done           bool       // Gives status of game, false if no, true if yes
 }
 
+// Initializes all variables held in Game
+// Fills board with '_'
+// Returns a Game
 func createGame() Game {
 	newGame := Game{}
 	newGame.board = [][]string{
@@ -20,6 +24,7 @@ func createGame() Game {
 	return newGame
 }
 
+// Prints board in a user-friendly format
 func printBoard(game Game) {
 	for i := 0; i < 6; i++ {
 		fmt.Println(game.board[i])
@@ -27,6 +32,8 @@ func printBoard(game Game) {
 	fmt.Println(" 1 2 3 4 5 6 7")
 }
 
+// Returns a boolean value if there is a draw (cannot add anymore pieces to the board)
+// Returns true if it's a draw, false if not
 func boardFilled(game Game) bool {
 	isFull := true
 	for i := 0; i < 7; i++ {
@@ -38,6 +45,8 @@ func boardFilled(game Game) bool {
 	return isFull
 }
 
+// Adds a piece to the board given the piece and column (Simulates piece being "dropped")
+// Returns an updated Game with piece added
 func addPiece(index int, piece string, game Game) Game {
 	for i := 5; i >= 0; i-- {
 		if game.board[i][index] == "_" {
@@ -49,8 +58,10 @@ func addPiece(index int, piece string, game Game) Game {
 	return game
 }
 
+//Checks if there is 4 same pieces in a row, column, or diagonal
+//Returns 1 (Player 1 wins), 2 (Player 2 wins), -1 (No one wins)
 func gameWin(game Game) int {
-	//horizontal check
+	// Horizontal check
 	for _, line := range game.board {
 		for i := 0; i <= 7-4; i++ {
 			if compare(line[i:i+4], "X") {
@@ -63,7 +74,7 @@ func gameWin(game Game) int {
 		}
 	}
 
-	//vertical check
+	// vertical check
 	for i := 0; i < 7; i++ {
 		currentRow := getVerticalLine(game, i)
 		for j := 0; j <= 7-4; j++ {
@@ -77,7 +88,7 @@ func gameWin(game Game) int {
 		}
 	}
 
-	//diagonal L-R check
+	// Diagonal L-R check
 	for i := 5; i >= 3; i-- {
 		for j := 0; j <= 3; j++ {
 			currentDiag := getDiagLR(game, i, j)
@@ -91,7 +102,7 @@ func gameWin(game Game) int {
 		}
 	}
 
-	//diagonal R-L check
+	// Diagonal R-L check
 	for i := 5; i >= 3; i-- {
 		for j := 6; j >= 3; j-- {
 			currentDiag := getDiagRL(game, i, j)
@@ -107,6 +118,8 @@ func gameWin(game Game) int {
 	return -1
 }
 
+// Helper function to compare a slice and a string to see if all elements in the slice are equal to the string
+// Returns true if yes, false if no
 func compare(slice []string, str string) bool {
 	if slice == nil {
 		return false
@@ -119,6 +132,8 @@ func compare(slice []string, str string) bool {
 	return true
 }
 
+// Helper function that adds a given column into a slice
+// Returns a slice that represents the column at number "col"
 func getVerticalLine(game Game, col int) []string {
 	returnSlice := []string{}
 	for i := 0; i < 6; i++ {
@@ -127,16 +142,18 @@ func getVerticalLine(game Game, col int) []string {
 	return returnSlice
 }
 
+// Helper function that adds a given LR diagonal into a slice given an x and y start point
+// Returns a slice that represents a LR diagonal that starts at the given start point
 func getDiagLR(game Game, x int, y int) []string {
 	returnSlice := []string{}
 	returnSlice = append(returnSlice, game.board[x][y], game.board[x-1][y+1], game.board[x-2][y+2], game.board[x-3][y+3])
 	return returnSlice
 }
 
+// Helper function that adds a given RL diagonal into a slice given an x and y start point
+// Returns a slice that represents a RL diagonal that starts at the given start point
 func getDiagRL(game Game, x int, y int) []string {
 	returnSlice := []string{}
 	returnSlice = append(returnSlice, game.board[x][y], game.board[x-1][y-1], game.board[x-2][y-2], game.board[x-3][y-3])
 	return returnSlice
 }
-
-
--- Run.go
+++ Run.go
@@ -3,40 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	g := createGame()
+	g := createGame() //Creates new instance of a blank Game
 	printBoard(g)
 	numTurns := 0
-	for !g.done {
+	for !g.done { // Loops until a win or draw
 		place := 0
-		if numTurns%2 == 0 {
+		if numTurns%2 == 0 { //Player 1 turn
 			fmt.Println("Player 1 turn")
 			fmt.Print("Column: ")
 			fmt.Scan(&place)
-			if place > 7 {
+			if place > 7 { //User inputs a number outside of the board
 				fmt.Println("Enter a column from 1-7")
 				continue
-			} else if g.piecesInColumn[place-1] >= 6 {
+			} else if g.piecesInColumn[place-1] >= 6 { //User inputs a column that is filled already
 				fmt.Println("Column is full, try another column")
 				continue
 			}
-			g = addPiece(place-1, "X", g)
-		} else {
+			g = addPiece(place-1, "X", g) // Adds a 'X' piece at inputted column
+		} else { // Player 2 turn
 			fmt.Println("Player 2 turn")
 			fmt.Print("Column: ")
 			fmt.Scan(&place)
-			if place > 7 {
+			if place > 7 { //User inputs a number outside of the board
 				fmt.Println("Enter a column from 1-7")
 				continue
-			} else if g.piecesInColumn[place-1] >= 6 {
+			} else if g.piecesInColumn[place-1] >= 6 { //User inputs a column that is filled already
 				fmt.Println("Column is full, try another column")
 				continue
 			}
-			g = addPiece(place-1, "O", g)
+			g = addPiece(place-1, "O", g) // Adds a 'O' piece at inputted column
 		}
 		printBoard(g)
 		fmt.Println()
 		numTurns = numTurns + 1
-		player := gameWin(g)
+		player := gameWin(g) //Checks if updated game has a win
 		if player == 1 {
 			fmt.Println("Player 1 Wins!")
 			break
